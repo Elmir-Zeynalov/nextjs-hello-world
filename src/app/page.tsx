58cadb382b50f60658d0d1ace1f09ac6422b3a7a
Elmir <elmir.zeynalov@soprasteria.com>
2025-12-01 23:58:23 +0100

    new p

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -17,8 +17,8 @@ export default async function IndexPage() {
   const posts = await client.fetch<SanityDocument[]>(POSTS_QUERY, {}, options);
 
   return (
-   <p> 
+    <main className="container mx-auto min-h-screen max-w-3xl p-8">
       Hei, jeg heter Elmir og jeg elsker planter!
-    </p>
+    </main>
   );
 }
